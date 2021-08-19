9517e2267d6c1ef87e652a510eef73a25d539aa0
BFDarian <bensfd99@gmail.com>
2021-08-19 14:41:17 +0100

    update schema 9

--- schema/cinema-schema.sql
+++ schema/cinema-schema.sql
@@ -20,7 +20,7 @@ CREATE TABLE cinema (
 CREATE TABLE film (
     film_id INT AUTO_INCREMENT,
     film_name VARCHAR(50) NOT NULL,
-    duration TIME NOT NULL,
+    duration VARCHAR(20) NOT NULL,
     director VARCHAR(50) NOT NULL,
     release_date DATE NOT NULL,
     PRIMARY KEY (film_id)
@@ -43,5 +43,9 @@ CREATE TABLE booking (
     FOREIGN KEY (fk_film_id) REFERENCES film(film_id)
 );
 
--- INSERT INTO film (film_name, duration, director, release_date)
--- VALUES ('Inglourious Bastards', '02:33:00.00000', Quentin Tarantino, '2009-08-19')
+
+
+--INSERT INTO customer(email, customer_name, customer_phoneNo)
+--VALUES('test1@fake.com', 'John Smith','07853254367'), ('test2@fake.com', 'Jane Doe','07743256301'), ('test3@fake.com', 'Harvey Jones','07744986321');
+--INSERT INTO film(film_name, duration, director, release_date)
+--VALUES ('Inglourious Bastards', '02:33:00.00000', Quentin Tarantino, '2009-08-19');
